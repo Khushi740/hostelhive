2d85f09ef4e32b47ce2f8a890db5192c8d108f80
ishitag22 <ishitagaur4@gmail.com>
2023-11-03 01:08:44 +0530

    Added content on Settings Screen
    Signed-off-by: ishitag22 <ishitagaur4@gmail.com>

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+
 class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
+  const SettingsPage({Key? key}) : super(key: key);
 
   @override
   State<SettingsPage> createState() => _SettingsPageState();
@@ -11,10 +12,93 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Settings",style: TextStyle(color: Colors.black),),
+        title: Text("Settings", style: TextStyle(color: Colors.black),),
         elevation: 0,
         backgroundColor: Colors.amberAccent,
       ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0), // Add padding from sides
+        child: Settings(),
+      ),
+    );
+  }
+}
+
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  bool dark = true;
+  bool per = true;
+  bool noti = true;
+
+  final MaterialStateProperty<Color?> trackColor = MaterialStateProperty.resolveWith<Color?>(
+        (Set<MaterialState> states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.amber;
+      }
+      return null;
+    },
+  );
+
+  final MaterialStateProperty<Color?> overlayColor = MaterialStateProperty.resolveWith<Color?>(
+        (Set<MaterialState> states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.amber.withOpacity(0.54);
+      }
+      if (states.contains(MaterialState.disabled)) {
+        return Colors.grey.shade400;
+      }
+      return null;
+    },
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        buildSwitchRow(Text("Dark Mode", style: TextStyle(fontWeight:FontWeight.w200),) as String, dark),
+        buildSwitchRow("Permissions", per),
+        buildSwitchRow("Push Notifications", noti),
+      ],
+    );
+  }
+
+  Widget buildSwitchRow(String text, bool value) {
+    return Card(
+    margin: EdgeInsets.symmetric(vertical: 8.0),
+      borderOnForeground: true,
+      shape: RoundedRectangleBorder(side: BorderSide(color: Colors.amber), borderRadius: BorderRadius.circular(20)),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+      child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(text),
+        Switch(
+          value: value,
+          overlayColor: overlayColor,
+          trackColor: trackColor,
+          thumbColor: MaterialStateProperty.all(Colors.black),
+          onChanged: (bool newValue) {
+            setState(() {
+              if (text == "Dark Mode") {
+                dark = newValue;
+              } else if (text == "Permissions") {
+                per = newValue;
+              } else if (text == "Push Notifications") {
+                noti = newValue;
+              }
+            });
+          },
+        ),
+      ],
+    ),
+    ),
     );
   }
 }
