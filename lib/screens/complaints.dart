0bdeb2ba9b9a8a4b4a31571d4c81b05aba6edc75
Himanshhiii <shimanshi924@gmail.com>
2023-11-21 12:42:17 +0530

    toasts and corner cases added

--- lib/screens/complaints.dart
+++ lib/screens/complaints.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get_connect/http/src/_http/_io/_file_decoder_io.dart';
-
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Complaints extends StatefulWidget {
   const Complaints({Key? key}) : super(key: key);
@@ -18,13 +18,15 @@ class ComplaintsScreenState extends State<Complaints> {
       appBar: AppBar(
         title: Text(
           "Complaints",
-          style: TextStyle(color: Colors.black,),
+          style: TextStyle(
+            color: Colors.black,
+          ),
         ),
         elevation: 0,
         backgroundColor: Colors.amberAccent,
       ),
       body: SingleChildScrollView(
-      child: ComplaintsForm(),
+        child: ComplaintsForm(),
       ),
     );
   }
@@ -56,134 +58,169 @@ class _ComplaintsFormState extends State<ComplaintsForm> {
       category = null;
       descriptionController.clear();
     });
-    ScaffoldMessenger.of(context).showSnackBar(
+    /* ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
           content: Container(padding: EdgeInsets.only(bottom: 16.0),
           child: Text("Complaint submitted successfully", style: TextStyle(backgroundColor: Colors.amber, color: Colors.black),),
       ),
       ),
-    );
+    ); */
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: [
-          DropdownButton<String>(
-            value: hostel,
-            hint: Text("Select Hostel"),
-            onChanged: (newvalue) {
-              setState(() {
-                hostel = newvalue;
-               // writeToFirestore(hostel);
-              });
-            },
-            items: hostelNumbers.map((hcategory) {
-              return DropdownMenuItem<String>(
-                value: hcategory,
-                child: Text(hcategory),
-              );
-            }).toList(),
-          ),
-          SizedBox(height: 16),
-          DropdownButton<int>(
-            value: room,
-            hint: Text("Room no"),
-            onChanged: (newRoom) {
-              setState(() {
-                room = newRoom;
-                //writeToFirestore('room');
-              });
-            },
-            items: List.generate(36, (index) {
-              return DropdownMenuItem<int>(
-                value: index + 1,
-                child: Text((index + 1).toString()),
-              );
-            }),
-          ),
-
-          SizedBox(height: 16),
-          DropdownButton<int>(
-            value: bed,
-            hint: Text("Bed no"),
-            onChanged: (newBed) {
-              setState(() {
-                bed = newBed;
-                //writeToFirestore('bed');
-              });
-            },
-            items: List.generate(4, (index) {
-              return DropdownMenuItem<int>(
-                value: index + 1,
-                child: Text((index + 1).toString()),
-              );
-            }),
-          ),
-
-          SizedBox(height: 16),
-          DropdownButton<String>(
-            value: category,
-            hint: Text("Select Category"),
-            onChanged: (newvalue) {
-              setState(() {
-                category = newvalue;
-                //writeToFirestore(category);
-              });
-            },
-            items: categories.map((ccategory) {
-              return DropdownMenuItem<String>(
-                value: ccategory,
-                child: Text(ccategory),
-              );
-            }).toList(),
-          ),
-          SizedBox(height: 16),
-          Container(
-      padding: EdgeInsets.symmetric(horizontal:16),
+      children: [
+        DropdownButton<String>(
+          value: hostel,
+          hint: Text("Select Hostel"),
+          onChanged: (newvalue) {
+            setState(() {
+              hostel = newvalue;
+              // writeToFirestore(hostel);
+            });
+          },
+          items: hostelNumbers.map((hcategory) {
+            return DropdownMenuItem<String>(
+              value: hcategory,
+              child: Text(hcategory),
+            );
+          }).toList(),
+        ),
+        SizedBox(height: 16),
+        DropdownButton<int>(
+          value: room,
+          hint: Text("Room no"),
+          onChanged: (newRoom) {
+            setState(() {
+              room = newRoom;
+              //writeToFirestore('room');
+            });
+          },
+          items: List.generate(36, (index) {
+            return DropdownMenuItem<int>(
+              value: index + 1,
+              child: Text((index + 1).toString()),
+            );
+          }),
+        ),
+        SizedBox(height: 16),
+        DropdownButton<int>(
+          value: bed,
+          hint: Text("Bed no"),
+          onChanged: (newBed) {
+            setState(() {
+              bed = newBed;
+              //writeToFirestore('bed');
+            });
+          },
+          items: List.generate(4, (index) {
+            return DropdownMenuItem<int>(
+              value: index + 1,
+              child: Text((index + 1).toString()),
+            );
+          }),
+        ),
+        SizedBox(height: 16),
+        DropdownButton<String>(
+          value: category,
+          hint: Text("Select Category"),
+          onChanged: (newvalue) {
+            setState(() {
+              category = newvalue;
+              //writeToFirestore(category);
+            });
+          },
+          items: categories.map((ccategory) {
+            return DropdownMenuItem<String>(
+              value: ccategory,
+              child: Text(ccategory),
+            );
+          }).toList(),
+        ),
+        SizedBox(height: 16),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 16),
           child: TextFormField(
             controller: descriptionController,
             decoration: InputDecoration(
               hintText: "Describe your complaint",
               border: OutlineInputBorder(),
-              focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber)),
             ),
             maxLines: 6,
           ),
+        ),
+        SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () {
+            writeToFirestore();
+            handleSubmit();
+          },
+          child: Text(
+            "Submit",
+            style: TextStyle(color: Colors.black),
           ),
-          SizedBox(height: 16),
-          ElevatedButton(
-            onPressed:(){
-               writeToFirestore( );
-               handleSubmit();
-            },
-            child: Text("Submit", style: TextStyle(color: Colors.black),),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.amber,
-            ),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.amber,
           ),
-        ],
+        ),
+      ],
       //),
-   // ),
+      // ),
       //  ),
-
-      );
+    );
   }
+
   void writeToFirestore() {
     if (hostel != null && room != null && bed != null && category != null) {
       FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-      firestore.collection('complaints').add({
-        'hostel': hostel,
-        'room': room,
-        'bed': bed,
-        'category': category,
-        'timestamp': FieldValue.serverTimestamp(),
-        'description': descriptionController.text,
-      })
-          .then((value) => print("Submitted"))
-          .catchError((error) => print("Failed to add data: $error"));
+      firestore
+          .collection('complaints')
+          .add({
+            'hostel': hostel,
+            'room': room,
+            'bed': bed,
+            'category': category,
+            'timestamp': FieldValue.serverTimestamp(),
+            'description': descriptionController.text,
+          })
+          .then((value) => Fluttertoast.showToast(
+                msg: "Submitted Successfully",
+                toastLength: Toast
+                    .LENGTH_SHORT, // Duration: Toast.LENGTH_SHORT or Toast.LENGTH_LONG
+                gravity: ToastGravity
+                    .BOTTOM, // Position: ToastGravity.TOP, ToastGravity.CENTER
+                timeInSecForIosWeb: 1, // Duration for iOS and web (in seconds)
+                backgroundColor: Colors.orange, // Background color
+                textColor: Colors.white, // Text color
+                fontSize: 16.0, // Font size
+              ))
+          .catchError((error) => Fluttertoast.showToast(
+                msg: "Error : $error",
+                toastLength: Toast
+                    .LENGTH_SHORT, // Duration: Toast.LENGTH_SHORT or Toast.LENGTH_LONG
+                gravity: ToastGravity
+                    .BOTTOM, // Position: ToastGravity.TOP, ToastGravity.CENTER
+                timeInSecForIosWeb: 1, // Duration for iOS and web (in seconds)
+                backgroundColor: Colors.orange, // Background color
+                textColor: Colors.white, // Text color
+                fontSize: 16.0, // Font size
+              ));
     } else {
-      print("Please fill in all the details");
+      Fluttertoast.showToast(
+        msg: "Enter all details",
+        toastLength: Toast
+            .LENGTH_SHORT, // Duration: Toast.LENGTH_SHORT or Toast.LENGTH_LONG
+        gravity: ToastGravity
+            .BOTTOM, // Position: ToastGravity.TOP, ToastGravity.CENTER
+        timeInSecForIosWeb: 1, // Duration for iOS and web (in seconds)
+        backgroundColor: Colors.orange, // Background color
+        textColor: Colors.white, // Text color
+        fontSize: 16.0, // Font size
+      );
     }
   }
 }
